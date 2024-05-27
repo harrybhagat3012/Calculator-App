ccdac0964f6d98799d22289cd062336f5bd9de9c
harrybhagat3012 <harshilbhagat30@gmail.com>
2024-05-27 14:27:29 +0530

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("org.mozilla:rhino:1.7.13");
 }
